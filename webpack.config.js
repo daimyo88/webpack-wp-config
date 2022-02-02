7ce252c0d416bd61524e1d559c149d82a5a01775
Denys Dmytruk <den.dmytruk@gmail.com>
2022-02-02 13:49:46 +0200

    correct files added

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,44 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-var path = require('path');
+const minify = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
-// change these variables to fit your project
-const jsPath= './assets/js';
-const cssPath = './assets/sass';
 const outputPath = 'dist';
-const localDomain = 'http://mysite.local';
-const entryPoints = {
-  // 'app' is the output name, people commonly use 'bundle'
-  // you can have more than 1 entry point
-  'app': jsPath + '/app.js',
-};
+const localDomain = 'http://theme.local';
 
 module.exports = {
-  entry: entryPoints,
-  output: {
-    path: path.resolve(__dirname, outputPath),
-    filename: '[name].js',
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-
-    // Uncomment this if you want to use CSS Live reload
-    /*
-    new BrowserSyncPlugin({
-      proxy: localDomain,
-      files: [ outputPath + '/*.css' ],
-      injectCss: true,
-    }, { reload: false, }),
-    */
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s?[c]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.sass$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: { indentedSyntax: true }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|woff|woff2|eot|ttf|svg)$/i,
-        use: 'url-loader?limit=1024'
-      }
-    ]
-  },
+	entry: {
+		js: './assets/js/app.js',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'scripts.js'
+	},
+	module: {
+		rules: [{
+			test:/\.(s*)css$/,
+			use: [
+				miniCss.loader,
+				'css-loader',
+				'sass-loader',
+			]
+		}]
+	},
+	optimization: {
+		minimizer: [
+			new minify({}),
+			new TerserPlugin(),
+		],
+	},
+	plugins: [
+		new miniCss({
+			filename: '../dist/styles.css',
+		}),
+		new BrowserSyncPlugin({
+			proxy: localDomain,
+			files: [ outputPath + '/*.css' ],
+			injectCss: true,
+		  }, { reload: false, }),
+	]
 };
